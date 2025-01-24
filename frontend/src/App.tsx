30149afd73bafec613ccf97bc22932534122ff05
Mouzone <sunnyliu010@gmail.com>
2025-01-24 12:21:41 -0500

    Set up filtering and searching funcitonality for data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,21 @@ import useSWR from 'swr';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
+const labels = {
+    firstName: "First Name",
+    lastName: "Last Name",
+    schoolYear: "Grade",
+    teacher: "Teacher"
+}
+
 function App() {
     const [searchValues, setSearchValues] = useState({
         firstName: "",
         lastName: "",
-        grade: null,
+        schoolYear: "",
         teacher: "",
     })
+    console.log(searchValues)
     const { data, error, isLoading } = useSWR('http://localhost:3000/students', fetcher)
     if (error) {
         return <div>Failed to load data.</div>
@@ -19,13 +27,20 @@ function App() {
         return <div>Loading...</div>
     }
 
+    const filtered = data.filter((entry) => {
+        return entry["firstName"].toLowerCase().includes(searchValues["firstName"].toLowerCase())
+            && entry["lastName"].toLowerCase().includes(searchValues["lastName"].toLowerCase())
+            &&  (!parseInt(searchValues["schoolYear"]) || entry["schoolYear"] === parseInt(searchValues["schoolYear"]))
+            && entry["afamTeacher"]["firstName"].toLowerCase().includes(searchValues["teacher"].toLowerCase())
+    })
+
     return (
         <>
             <div>
                 {
                     Object.entries(searchValues).map(([key, value]) => (
                         <>
-                            <label>{key}</label>
+                            <label>{labels[key]}</label>
                             <input key={key} value={value} onChange={(e) => setSearchValues({...searchValues, [key]: e.target.value})}/>
                         </>
                     ))
@@ -42,7 +57,7 @@ function App() {
                 </thead>
                 <tbody>
                     {
-                        data.map((student) => (
+                        filtered.map((student) => (
                             <tr key={student.id}>
                                 <td>{student.firstName}</td>
                                 <td>{student.lastName}</td>
