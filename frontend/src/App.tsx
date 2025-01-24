bb93e178fc8b69c99e3a1db78f82097da1bd4e11
Mouzone <sunnyliu010@gmail.com>
2025-01-23 19:56:49 -0500

    Add inputs to be able to search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
         grade: null,
         teacher: "",
     })
-
     const { data, error, isLoading } = useSWR('http://localhost:3000/students', fetcher)
     if (error) {
         return <div>Failed to load data.</div>
@@ -21,28 +20,40 @@ function App() {
     }
 
     return (
-    <table>
-        <thead>
-            <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>School Year</th>
-                <th>Teacher</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                data.map((student) => (
-                    <tr key={student.id}>
-                        <td>{student.firstName}</td>
-                        <td>{student.lastName}</td>
-                        <td>{student.schoolYear}</td>
-                        <td>{student.afamTeacher.firstName}</td>
+        <>
+            <div>
+                {
+                    Object.entries(searchValues).map(([key, value]) => (
+                        <>
+                            <label>{key}</label>
+                            <input key={key} value={value} onChange={(e) => setSearchValues({...searchValues, [key]: e.target.value})}/>
+                        </>
+                    ))
+                }
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>School Year</th>
+                        <th>Teacher</th>
                     </tr>
-                ))
-            }
-        </tbody>
-    </table>
+                </thead>
+                <tbody>
+                    {
+                        data.map((student) => (
+                            <tr key={student.id}>
+                                <td>{student.firstName}</td>
+                                <td>{student.lastName}</td>
+                                <td>{student.schoolYear}</td>
+                                <td>{student.afamTeacher.firstName}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
 
