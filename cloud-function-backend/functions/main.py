ba88ce171c66237e511bf9308aed4b35fe503504
Mouzone <sunnyliu010@gmail.com>
2025-01-31 18:15:05 -0500

    Write PUT method to update student

--- cloud-function-backend/functions/main.py
+++ cloud-function-backend/functions/main.py
@@ -31,7 +31,7 @@ def getCollection(req: https_fn.Request) -> https_fn.Response:
         print(f"Error fetching documents: {e}")
         return https_fn.Response(jsonify({"error": "Internal Server Error"}), status=500)
 
-@https_fn.on_request(cors=options.CorsOptions(cors_methods=["POST", "PUT"]))
+@https_fn.on_request(cors=options.CorsOptions(cors_methods=["POST"]))
 def createStudent(req: https_fn.Request) -> https_fn.Response:
     try:
         if not req.data:
@@ -54,12 +54,40 @@ def createStudent(req: https_fn.Request) -> https_fn.Response:
             "age": age
         })
 
-        return  https_fn.Response(jsonify({"message": "Data processed successfully", "id": doc_ref.id}), status=200)
+        return https_fn.Response(jsonify({"message": "Data processed successfully", "id": doc_ref.id}), status=200)
 
     except Exception as e:
         print(f"Error processing request: {e}")
         return https_fn.Response(jsonify({"error": "Internal Server Error"}), status=500)
-        
+
+@https_fn.on_request(cors=https_fn.CorsOptions(cors_methods=["PUT"]))
+def editStudent(req: https_fn.Request) -> https_fn.Response:
+    try:
+        if not req.data:
+            return https_fn.Response(jsonify({"error": "Request body is empty"}), status=400)
+
+        request_data = req.get_json()
+
+        if not request_data:
+            return https_fn.Response(jsonify({"error": "Invalid JSON"}), status=400)
+
+        document_id = request_data.get("document_id")
+        if not document_id:
+            return https_fn.Response(jsonify({"error": "Missing 'document_id' field"}), status=400)
+
+        doc_ref = fireStore_client.collection("students").document(document_id)
+
+        if not doc_ref.get().exists:
+            return https_fn.Response(jsonify({"error": "Document does not exist"}), status=404)
+
+        update_data = {k: v for k, v in request_data.items() if k != "document_id"}
+        doc_ref.update(update_data)
+
+        return https_fn.Response(jsonify({"message": "Document updated successfully", "id": document_id}), status=200)
+
+    except Exception as e:
+        print(f"Error processing request: {e}")
+        return https_fn.Response(jsonify({"error": "Internal Server Error"}), status=500)
 # Routes:
 ## Create Student / Edit Student
 ### can combine since both send the same payload, of a ton of data
