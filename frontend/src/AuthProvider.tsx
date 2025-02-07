67360ed12ba15c7211714ac8ba75eaade34d0392
Mouzone <sunnyliu010@gmail.com>
2025-02-07 12:18:28 -0500

    Refactor auhtprovider component

--- frontend/src/AuthProvider.tsx
+++ frontend/src/AuthProvider.tsx
@@ -10,40 +10,40 @@ const AuthContext = createContext<AuthContextType>({
     setToken: () => {},
 })
 
-export const AuthProvider = ({ children }: {children: React.ReactNode}) => {
-    const [user, setUser] = useState<User| null>(null)
-    const [token, setToken] = useState<string | null>(null)
-    const auth = getAuth(app)
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [token, setToken] = useState<string | null>(null);
+    const auth = getAuth(app);
 
     useEffect(() => {
-        const unsubscribeAuth = onAuthStateChanged(auth, async (user) => {
+        const combinedUnsubscribe = onAuthStateChanged(auth, async (user) => {
+            setUser(user); // Update user state
+
             if (user) {
-                setUser(user)
-                const idToken = await user.getIdToken()
-                setToken(idToken)
+                const idToken = await user.getIdToken();
+                setToken(idToken); // Update token state
             } else {
-                setUser(null)
-                setToken(null)
+                setToken(null);
             }
-        })
-        
-        const unsubscribeToken = onIdTokenChanged(auth, async (user) => {
+        });
+
+        onIdTokenChanged(auth, async (user) => {
             if (user) {
-                const idToken = await user.getIdToken()
-                setToken(idToken)
+              const idToken = await user.getIdToken();
+              setToken(idToken);
+            } else {
+              setToken(null);
             }
-        })
-        return () => {
-            unsubscribeAuth()
-            unsubscribeToken()
-        }
-    }, [auth])
+        });
+
+        return () => combinedUnsubscribe(); // Combined unsubscribe
+    }, [auth]);
 
     return (
-        <AuthContext.Provider value = {{user, setUser, token, setToken}}>
+        <AuthContext.Provider value={{ user, setUser, token, setToken }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
 
 export const useAuth = () => useContext(AuthContext)
