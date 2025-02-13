4e7ca68bf629a225bfeae8295e1da8fb6a3db024
Mouzone <sunnyliu010@gmail.com>
2025-02-13 12:44:12 -0500

    Properly format phone number input to work with users that already submtited phone numbers with dashes, and users that have phone numbers without dashes

--- frontend/src/Inputs/PhoneInput.tsx
+++ frontend/src/Inputs/PhoneInput.tsx
@@ -1,15 +1,31 @@
 export default function PhoneInput({label, value, name, onChange, disabled}: {label: string, value: string, name: string, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void, disabled: boolean}){
+    // if key is not number then don't change
+    // for value add dashes after first two set of three digits?
+
+    const formatPhoneNumber = (value: string) => {
+        const cleaned = value.replace(/[^0-9]/g, '');
+        let formatted = '';
+        for (let i = 0; i < cleaned.length; i++) {
+            if (i === 3 || i === 6) {
+                formatted += '-';
+            }
+            formatted += cleaned[i];
+        }
+        return formatted;
+    }
+
     return (
         <div className="flex flex-col">
             <label className="font-bold">{label}</label>
             <input
-                type="tel"
+                type="text"
                 name={name}
-                value={value}
+                value={formatPhoneNumber(value)}
                 onChange={onChange}
                 className="border border-gray-300 rounded p-2"
                 required
                 disabled={disabled}
+                maxLength={12}
             />
         </div>
     )
