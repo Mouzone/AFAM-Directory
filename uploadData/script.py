d334e2ebf05d76b5d021e31a270ded878aace2d9
Mouzone <sunnyliu010@gmail.com>
2025-02-13 10:54:27 -0500

    Add .strip() to all values and .lower() to allergy fields, and add Unassigned for students with no teacher

--- uploadData/script.py
+++ uploadData/script.py
@@ -36,9 +36,9 @@ def csv_to_firestore(csv_file_path, collection_name, batch_size):
 
                 # Convert all values to appropriate types.  Critical for Firestore!
                 converted_row = {}
-                converted_row["firstName"] = row["First Name"]
-                converted_row["lastName"] = row["Last Name"]
-                converted_row["schoolYear"] = row["Grade"]
+                converted_row["firstName"] = row["First Name"].strip()
+                converted_row["lastName"] = row["Last Name"].strip()
+                converted_row["schoolYear"] = row["Grade"].strip()
 
                 # to go from 1/17/2009 to 2025-02-28
                 dob_parts = row["Date of Birth (DOB)"].split("/")
@@ -47,40 +47,39 @@ def csv_to_firestore(csv_file_path, collection_name, batch_size):
                 if row["Allergies"] == "":
                     converted_row["allergies"] = []
                 else:
-                    converted_row["allergies"] = row["Allergies"].split(",")
-                converted_row["email"] = row["Personal Email Address"]
-                converted_row["phoneNumber"] = row["Personal Phone Number"]
-                converted_row["gender"] = row["Gender"]
-                converted_row["highSchool"] = row["High School"]
+                    converted_row["allergies"] = [allergy.strip().lower() for allergy in row.get("Allergies", "").split(",")]
+                converted_row["email"] = row["Personal Email Address"].strip()
+                converted_row["phoneNumber"] = row["Personal Phone Number"].strip()
+                converted_row["gender"] = row["Gender"].strip()
+                converted_row["highSchool"] = row["High School"].strip()
                 converted_row["home"] = {
-                    "city": row["City"],
-                    "streetAddress": row["Street Address"],
-                    "zipCode": row["Postal Code"]
+                    "city": row["City"].strip(),
+                    "streetAddress": row["Street Address"].strip(),
+                    "zipCode": row["Postal Code"].strip()
                 }
 
                 converted_row["guardian1"] = {
-                    "email": row["Parent 1 Email Address"],
-                    "firstName": row["Parent 1 First Name"],
-                    "lastName": row["Parent 1 Last Name"],
-                    "phoneNumber": row["Parent 1 Phone Number"]
+                    "email": row["Parent 1 Email Address"].strip(),
+                    "firstName": row["Parent 1 First Name"].strip(),
+                    "lastName": row["Parent 1 Last Name"].strip(),
+                    "phoneNumber": row["Parent 1 Phone Number"].strip()
                 }
                 converted_row["guardian2"] = {
-                    "email": row["Parent 2 Email Address"],
-                    "firstName": row["Parent 2 First Name"],
-                    "lastName": row["Parent 2 Last Name"],
-                    "phoneNumber": row["Parent 2 Phone Number"]
+                    "email": row["Parent 2 Email Address"].strip(),
+                    "firstName": row["Parent 2 First Name"].strip(),
+                    "lastName": row["Parent 2 Last Name"].strip(),
+                    "phoneNumber": row["Parent 2 Phone Number"].strip()
                 }
                 # from 10A Sunny Liu to {Sunny, Liu}
                 teacher_parts = row["Small Group Class"].split(" ")
                 if len(teacher_parts) == 3:
                     converted_row["teacher"] = {
-                        "firstName": teacher_parts[1],
-                        "lastName": teacher_parts[2]
+                        "firstName": teacher_parts[1].strip(),
+                        "lastName": teacher_parts[2].strip()
                     }
                 else:
                     converted_row["teacher"] = {
-                        "firstName": "",
-                        "lastName": ""
+                        "firstName": "Unassigned"
                     }
 
                 batch.set(doc_ref, converted_row)
