aada8b67f124cfeee3c4ea7c0ea4ce5d15d3bd5f
Mouzone <sunnyliu010@gmail.com>
2025-02-13 12:51:01 -0500

    Format zip code to only recieve numbers and limit to length 5

--- frontend/src/Inputs/TextInput.tsx
+++ frontend/src/Inputs/TextInput.tsx
@@ -1,16 +1,22 @@
 export default function TextInput({label, value, name, onChange, disabled}: {label: string, value: string, name: string,onChange: (e: React.ChangeEvent<HTMLInputElement>) => void, disabled: boolean}){
+    const formatZipCode = (value: string) => {
+        const cleaned = value.replace(/[^0-9]/g, ''); // Remove non-numeric chars
+        return cleaned.slice(0, 5); // Limit to 5 digits
+      }
+
     return (
         <div className="flex flex-col">
-                <label className="font-bold">{label}</label>
-                <input
-                    type="text"
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                    className="border border-gray-300 rounded p-2"
-                    required
-                    disabled={disabled}
-                />
-            </div>
+            <label className="font-bold">{label}</label>
+            <input
+                type="text"
+                name={name}
+                value={label === "Zip Code" ? formatZipCode(value) : value}
+                onChange={onChange}
+                className="border border-gray-300 rounded p-2"
+                required
+                disabled={disabled}
+                maxLength={label === "Zip Code" ? 5 : undefined}
+            />
+        </div>
     )
 }
