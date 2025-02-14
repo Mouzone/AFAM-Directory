84dd98f7f5033ea81bb9dedcb1992817b2c35581
Mouzone <sunnyliu010@gmail.com>
2025-02-13 20:24:27 -0500

    Remove old backends

--- backend/server/src/fireside.ts
+++
@@ -1,9 +0,0 @@
-// fireside.js
-import admin = require('firebase-admin');
-
-export function initializeFirebaseAdmin() {
-    const serviceAccount = require('../afam-directory-firebase-adminsdk-fbsvc-c6f23bf9b8.json');
-    return admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
-}
