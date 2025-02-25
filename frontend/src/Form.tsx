c6a606fb09d202fda1492881d084c9da19205af4
Mouzone <sunnyliu010@gmail.com>
2025-02-25 18:16:21 -0500

    Refactor functions in Form to work properly with generalData and privateData

--- frontend/src/Form.tsx
+++ frontend/src/Form.tsx
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { StudentGeneralInfo, StudentPrivateInfo, Teacher } from "./types";
-import {
-    addDoc,
-    collection,
-    deleteDoc,
-    doc,
-    updateDoc,
-    getDoc
-} from "firebase/firestore";
+import { collection, doc, getDoc, setDoc, deleteDoc } from "firebase/firestore";
 import {
     ref,
     uploadBytesResumable,
@@ -24,63 +17,51 @@ interface FormProps {
     teachers: Teacher[] | undefined;
 }
 
-export default function Form({generalState, closeForm, teachers}: FormProps) {
-    const [generalData, setGeneralData] = useState<StudentGeneralInfo>(generalState);
-    const [privateData, setPrivateData] = useState<StudentPrivateInfo>(studentPrivateInfoDefault);
+export default function Form({ generalState, closeForm, teachers }: FormProps) {
+    const [generalData, setGeneralData] =
+        useState<StudentGeneralInfo>(generalState);
+    const [privateData, setPrivateData] = useState<StudentPrivateInfo>(
+        studentPrivateInfoDefault
+    );
     const [isEdit, setIsEdit] = useState(false);
 
     const [image, setImage] = useState<File | null>(null);
     const [imageUrl, setImageUrl] = useState<string | null>(null);
     const [error, setError] = useState<string | null>(null);
 
-    const disabled = "id" in formData && !isEdit;
+    const disabled = "id" in generalData && !isEdit;
 
     useEffect(() => {
-        if (!formData["id"]) {
+        if (!generalData["id"]) {
             return;
         }
         async function fetchPrivateInfo() {
-            // if (!formData["id"]) {
-            //   setPrivateInfo(null); // Clear previous data if ID is missing
-            //   return;
-            // }
-      
-            const studentRef = doc(db, "students", formData["id"]);
+            const studentRef = doc(db, "students", generalData["id"] as string);
             const privateCollectionRef = collection(studentRef, "private");
             const privateDocRef = doc(privateCollectionRef, "privateInfo");
             const privateDocSnapshot = await getDoc(privateDocRef);
-            console.log(privateDocSnapshot.data())
+            setPrivateData(privateDocSnapshot.data() as StudentPrivateInfo);
         }
-        fetchPrivateInfo()
 
-        const headshotRef = ref(storage, `images/${formData["id"]}`);
-        getDownloadURL(headshotRef)
-            .then((url) => {
-                setImageUrl(url);
-            })
-            .catch((error) => {
-                console.error("Error getting download URL:", error);
-            });
+        async function fetchHeadshot() {
+            const headshotRef = ref(storage, `images/${generalData["id"]}`);
+            const url = await getDownloadURL(headshotRef);
+            setImageUrl(url);
+        }
+
+        fetchHeadshot;
+        fetchPrivateInfo();
     }, []);
 
-    // todo: delete image as well
-    const onDelete = () => {
-        const docRef = doc(db, "students", formData["id"] as string);
-        deleteDoc(docRef)
-            .then(() => {
-                console.log("Document successfully deleted!");
-            })
-            .catch((error) => {
-                console.error("Error deleting document: ", error);
-            });
-
+    const onDelete = async () => {
+        const docRef = doc(db, "students", generalData["id"] as string);
         const headshotRef = ref(storage, `images/${docRef.id}`);
-        deleteObject(headshotRef)
-            .then(() => console.log("Image delete", docRef.id))
-            .catch((error) => console.error(error));
+
+        await deleteDoc(docRef);
+        await deleteObject(headshotRef);
+
         closeForm();
     };
-    
 
     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (!e.target.files) {
@@ -117,40 +98,37 @@ export default function Form({generalState, closeForm, teachers}: FormProps) {
         );
     }
 
+    // runs the same regardless of "add" or "edit" scenario
     const onSubmit = async () => {
-        if (!generalData["id"]) {
-            const generalColRef = collection(db, "students");
-            const docRef = await addDoc(generalColRef, generalData)
-            const privateColRef = collection(docRef, "private")
-            await addDoc(privateColRef, privateData)
-        } else {
-            const docRef = doc(db, "students", generalData["id"]);
-            const privateColRef = collection(docRef, "private")
-            const privateDocRef = doc(privateColRef, "private", "privateInfo")
-            
-            await updateDoc(docRef, generalData)
-            await updateDoc(privateDocRef, privateData)
-        }
+        // type "generalData["id"]" as string since, fireStore will autogenerate a new id if it doesnt exist
+        const docRef = doc(db, "students", generalData["id"] as string);
+        const privateColRef = collection(docRef, "private");
+        const privateDocRef = doc(privateColRef, "private", "privateInfo");
+
+        await setDoc(docRef, generalData, { merge: true });
+        await setDoc(privateDocRef, privateData, { merge: true });
+
+        uploadImage(docRef.id);
         closeForm();
     };
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (
-            !formData.firstName ||
-            !formData.lastName ||
-            !formData.schoolYear ||
-            !formData.dob ||
-            !formData.gender ||
-            !formData.highSchool ||
-            !formData.phoneNumber ||
-            !formData.email
+            !generalData.firstName ||
+            !generalData.lastName ||
+            !generalData.schoolYear ||
+            !generalData.dob ||
+            !generalData.gender ||
+            !generalData.highSchool ||
+            !privateData.phoneNumber ||
+            !privateData.email
         ) {
             alert("Please fill out all required fields.");
             return;
         }
 
-        onSubmit(formData);
+        onSubmit();
     };
 
     return (
@@ -185,11 +163,11 @@ export default function Form({generalState, closeForm, teachers}: FormProps) {
                 )}
             </div>
 
-            <General/>
-            <Private/>
+            <General />
+            <Private />
 
             <Buttons
-                type={!("id" in formData) ? "add" : "view"}
+                type={!("id" in generalData) ? "add" : "view"}
                 isEdit={isEdit}
                 onDelete={onDelete}
                 setIsEdit={setIsEdit}
