2fc9aca0bc90c206045f68a4c535ef567f48603c
Mouzone <sunnyliu010@gmail.com>
2025-02-28 18:56:15 -0500

    Fix package error and add typing for cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,17 @@
 import { https } from "firebase-functions";
 import * as admin from "firebase-admin"; // Import the whole admin module
 
+type AccountData = {
+    email?: string,
+    password?: string,
+    role?: string
+}
+
+type Role = "pastor" | "teacher" | "deacon" | "student" | "welcome team leader"
+type RoleData = {
+    role?: Role,
+}
+
 admin.initializeApp();
 const authAdmin = admin.auth();
 
@@ -8,7 +19,7 @@ const authAdmin = admin.auth();
 // if it is authenticated: then check role 
 // hardcode check that role is pastor, deacon, student, teacher, welcome team leader
 export const createUserWithRole = https.onCall(async (data, _context) => {
-    const { email, password, role } = data;
+    const { email, password, role } = data as AccountData;
 
     if (!email || !password || !role) {
         throw new https.HttpsError(
@@ -28,12 +39,16 @@ export const createUserWithRole = https.onCall(async (data, _context) => {
         return { result: `User ${userRecord.uid} created with role ${role}.` };
     } catch (error) {
         console.error("Error creating user:", error);
-        throw new https.HttpsError("internal", error.message);
+        if (error instanceof Error) {
+            throw new https.HttpsError("internal", error.message);
+        }
+        throw new https.HttpsError("internal", "An unknown error occurred."); 
     }
 });
 
+
 export const generateInviteLink = https.onCall(async (data, _context) => {
-    const { role } = data;
+    const { role } = data as RoleData;
 
     if (!role) {
         throw new https.HttpsError(
@@ -43,11 +58,14 @@ export const generateInviteLink = https.onCall(async (data, _context) => {
     }
 
     try {
-        const token = await authAdmin.createCustomToken({ role });
+        const token = await authAdmin.createCustomToken("deez", { role });
         const link = `https://your-app.com/signup?token=${token}`;
         return { link };
     } catch (error) {
         console.error("Error generating invite link:", error);
-        throw new https.HttpsError("internal", error.message);
+        if (error instanceof Error) {
+            throw new https.HttpsError("internal", error.message);
+        }
+        throw new https.HttpsError("internal", "An unknown error occurred.");
     }
 });
