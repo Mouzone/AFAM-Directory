0343e3263359b58c999f50bdbbb22c61c105a282
Mouzone <sunnyliu010@gmail.com>
2025-03-05 11:54:12 -0500

    Refactor signup page to reduce any logic, and move all authorization logic to the backend

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,49 +1,45 @@
 "use client"
-import { auth } from "@/utility/firebase"
-import { signInWithCustomToken } from "firebase/auth"
+
+import { createUserWithRole } from "@/utility/cloud-functions"
 import { useRouter, useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Page(){
-    // check ?token={token}
-    // get role
-    // if token not valid redirect to login
-    // call cloud function create new, then redirect to login
     const router = useRouter()
     const searchParams = useSearchParams()
-    const [loading, setLoading] = useState(true)
+
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
     const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    
     const token = searchParams.get("token")
 
-    useEffect(() => {
-        if (token) {
-            verifyToken(token)
-        } else {
-            setLoading(false)
-            router.push("/")
-        }
-    })
+    if (!token) {
+        router.push("/")
+    }
 
-    const verifyToken = async (token: string) => {
+    const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
         try {
-            await signInWithCustomToken(auth, token)
-        } catch (e) {
-            setErrorMessage("Invalid or expired token")
-        } finally {
-            setLoading(false)
+            const response = await createUserWithRole({ email, password, token})
+            // if error show error message, else show success and redirect to login
+            console.log(response)
+        } catch (error) {
+            console.error('Error creating user:', error);
+            if (error instanceof Error) {
+                setErrorMessage(error.message);
+            }
         }
     }
 
-    if (loading) {
-        return <p> Loading... </p>
-    }
-
-    if (errorMessage) {
-        return <p> {errorMessage} </p>
-    }
     return (
-        <div>
-            { token }
-        </div>
+        <>
+            <form onSubmit={handleSignup}>
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Create Account</button>
+            </form>
+            { errorMessage && <p> {errorMessage} </p> }
+        </>
     )
 }
