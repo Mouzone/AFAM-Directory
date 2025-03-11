23ccf701a0c493e806462d66753bc000fccb5eec
Mouzone <sunnyliu010@gmail.com>
2025-03-11 12:05:07 -0400

    Create wokring script to create test users

--- modify firestore/addTestUsers.py
+++ modify firestore/addTestUsers.py
@@ -1,9 +1,3 @@
-# probably best to create a csv with info
-# all of them is just firstName, lastName, email, password, role
-# create the user in auth
-# fetch the uid
-# insert into organization > roles > {role}
-## document name is uid and add lastName, firstName, email to the doc
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import auth
@@ -11,34 +5,50 @@ from firebase_admin import auth
 import random
 import string
 
-import firebase_admin.auth
-
+firebase_admin.initialize_app()
 ROLES = ["pastor", "deacon", "welcome team leader", "teacher", "student"]
 
 
-def add_test_users(csv_file_path):
-    firebase_admin.initialize_app()
+def add_test_users():
     db = firestore.client()
 
-    organization_ref = db.collection("organization")
-    document_ref = organization_ref.document("roles")
     seen_names = set()
     for role in ROLES:
         for i in range(20):
-            # randomize two characters, compare it in set
-            # use the two characters as email
-            # create account
-            # create document
-            firstLetter = random.choice(string.ascii_letters)
-            secondLetter = random.choice(string.ascii_letters)
+            first_letter = random.choice(string.ascii_lowercase)
+            second_letter = random.choice(string.ascii_lowercase)
 
-            while (firstLetter, secondLetter) in seen_names:
-                firstLetter = random.choice(string.ascii_letters)
-                secondLetter = random.choice(string.ascii_letters)
+            while (first_letter, second_letter) in seen_names:
+                first_letter = random.choice(string.ascii_lowercase)
+                second_letter = random.choice(string.ascii_lowercase)
 
-            firstName = firstLetter
-            lastName = secondLetter
-            email = "{firstLetter}@{secondLetter}"
+            seen_names.add((first_letter, second_letter))
+
+            first_name = first_letter
+            last_name = second_letter
+            email = f"{first_letter}{second_letter}@gmail.com"
             password = "123456"
 
-            firebase_admin.auth.create_user()
+            user = auth.create_user(email=email, password=password)
+            user_doc_ref = db.document("organization", "roles", role, user.uid)
+            user_doc_ref.set(
+                {
+                    "firstName": first_name,
+                    "lastName": last_name,
+                    "email": email,
+                }
+            )
+
+
+# forgot to set auth local emulator, so have to remove all accounts from cloud
+def delete_test_users():
+    page = auth.list_users()
+    while page:
+        for user in page.users:
+            if len(user.email) == 12:
+                auth.delete_user(user.uid)
+        page = page.get_next_page()
+
+
+# delete_test_users()
+add_test_users()
