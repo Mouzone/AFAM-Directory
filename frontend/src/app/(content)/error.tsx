742438397c6ac589a91103b654e85ac36f7a3964
Mouzone <sunnyliu010@gmail.com>
2025-03-13 11:24:17 -0400

    Add error handling if a user does not have authentication and update layour to render accounts tab based on role

---
+++ frontend/src/app/(content)/error.tsx
@@ -0,0 +1,8 @@
+export default function ErrorPage() {
+    return (
+        <div>
+            <h1>An Error Occurred</h1>
+            <p>Something went wrong. Please try again later.</p>
+        </div>
+    );
+}
--- frontend/src/app/(content)/layout.tsx
+++ frontend/src/app/(content)/layout.tsx
@@ -2,23 +2,47 @@
 import Link from "next/link";
 import { auth } from "@/utility/firebase";
 import { signOut } from "firebase/auth";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Role } from "@/types";
+import { useRouter } from "next/navigation";
 
 type ContentLayoutProps = { children: React.ReactNode };
 export default function ContentLayout({ children }: ContentLayoutProps) {
+    const [showAccounts, setShowAccounts] = useState(false);
+    const router = useRouter();
+    useEffect(() => {
+        async function getRole() {
+            const tokenResult = await auth.currentUser?.getIdTokenResult();
+            if (!tokenResult) {
+                return router.push("/error");
+            }
+            const userRole = tokenResult.claims.role as Role;
+            // todo: redirect to an error page
+            const showAccounts = [
+                "pastor",
+                "welcome team leader",
+                "admin",
+            ].includes(userRole);
+            setShowAccounts(showAccounts);
+        }
+        getRole();
+    }, []);
+
     return (
         <div className="flex w-full justify-center gap-9">
             <nav className="flex flex-col justify-start pt-20">
                 <Link href="/directory" className="justify-end">
                     Directory
                 </Link>
-                <Link href="/accounts" className="justify-end">
-                    Accounts
-                </Link>
+                {showAccounts && (
+                    <Link href="/accounts" className="justify-end">
+                        Accounts
+                    </Link>
+                )}
                 <button
                     type="button"
                     onClick={() => signOut(auth)}
-                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" // Tailwind styles
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
                 >
                     Sign Out
                 </button>
