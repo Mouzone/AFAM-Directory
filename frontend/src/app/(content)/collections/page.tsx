c5b852f03b57ef9d355da40e4f6d955f2d036c60
Mouzone <sunnyliu010@gmail.com>
2025-03-28 18:55:49 -0400

    Scaffold collections page

--- frontend/src/app/(content)/collections/page.tsx
+++ frontend/src/app/(content)/collections/page.tsx
@@ -1,3 +1,7 @@
 export default function Page({}) {
+    // bounce if not verified
+
+    // fetch all collections the user owns
+    // then render name, ownner, update date in a card style component
     return <></>;
 }
