569155fcd9182bc3724e1e70b23c75297c7354a6
Mouzone <sunnyliu010@gmail.com>
2025-04-01 11:45:23 -0400

    Add firebase login and error display for login page

--- new-frontend/src/app/page.tsx
+++ new-frontend/src/app/page.tsx
@@ -1,37 +1,61 @@
 "use client";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { auth } from "./utility/firebase";
 
 export default function Page() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const submitHandler = async () => {
-        const response = await signInWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
-        console.log(response);
-    };
+    const [error, setError] = useState("");
+
+    async function submitHandler(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+            const response = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+        } catch (e) {
+            if (e instanceof Error) {
+                console.log(e);
+                setError(e.message);
+            }
+        }
+    }
     return (
         <form
             className="w-screen h-screen flex justify-center items-center"
-            onSubmit={submitHandler}
+            onSubmit={(e) => submitHandler(e)}
         >
             <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
                 <legend className="fieldset-legend">Login</legend>
 
                 <label className="fieldset-label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
+                <input
+                    type="email"
+                    className="input"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
 
                 <label className="fieldset-label">Password</label>
                 <input
                     type="password"
                     className="input"
                     placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
-                <button type="submit" className="btn btn-neutral mt-4">
+                <p className="text-center bg-red-200 text-red-400 rounded-2xl p-2 my-2">
+                    {error}
+                </p>
+                <button
+                    type="submit"
+                    className="btn btn-neutral"
+                    disabled={email === "" || password === ""}
+                >
                     Login
                 </button>
             </fieldset>
