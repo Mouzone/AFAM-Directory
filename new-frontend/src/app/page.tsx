817661a3b7d00f97ead29f78531031f35535d480
Mouzone <sunnyliu010@gmail.com>
2025-04-01 11:32:32 -0400

    Write submithandler for login and wrap using Form element for classic Form functionality

--- new-frontend/src/app/page.tsx
+++ new-frontend/src/app/page.tsx
@@ -1,38 +1,40 @@
 "use client";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
+import { auth } from "./utility/firebase";
 
 export default function Page() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const submitHandler = () => {};
+    const submitHandler = async () => {
+        const response = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        console.log(response);
+    };
     return (
-        <div className="w-screen h-screen flex justify-center items-center">
-            <fieldset
-                className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box"
-                onSubmit={submitHandler}
-            >
+        <form
+            className="w-screen h-screen flex justify-center items-center"
+            onSubmit={submitHandler}
+        >
+            <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
                 <legend className="fieldset-legend">Login</legend>
 
                 <label className="fieldset-label">Email</label>
-                <input
-                    type="email"
-                    className="input"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
+                <input type="email" className="input" placeholder="Email" />
 
                 <label className="fieldset-label">Password</label>
                 <input
                     type="password"
                     className="input"
                     placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
                 />
-
-                <button className="btn btn-neutral mt-4">Login</button>
+                <button type="submit" className="btn btn-neutral mt-4">
+                    Login
+                </button>
             </fieldset>
-        </div>
+        </form>
     );
 }
