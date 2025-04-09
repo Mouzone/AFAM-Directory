50f946faa4d74bc3095ebd469205cae2abf13ec7
Mouzone <sunnyliu010@gmail.com>
2025-04-09 11:14:35 -0400

    Make x button reset state and move it to the form

--- new-frontend/src/components/Forms/CreateStudentForm.tsx
+++ new-frontend/src/components/Forms/CreateStudentForm.tsx
@@ -7,7 +7,7 @@ import {
     generalFormDataDefault,
     privateFormDataDefault,
 } from "@/utility/consts";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 import { db } from "@/utility/firebase";
 
 export default function CreateStudentForm() {
@@ -47,83 +47,99 @@ export default function CreateStudentForm() {
     };
 
     return (
-        <form onSubmit={(e) => onSubmit(e)}>
-            {/* headshot */}
-            <div className="tabs tabs-lift">
-                <input
-                    type="radio"
-                    name="general"
-                    className="tab"
-                    aria-label="General"
-                    checked={tab === "general"}
-                    onChange={() => setTab("general")}
-                />
-                <div className="tab-content bg-base-100 border-base-300 p-6">
-                    <GeneralSubForm
-                        data={generalFormData}
-                        changeData={(field: string, value: string) =>
-                            setGeneralFormData({
-                                ...generalFormData,
-                                [field]: value,
-                            })
-                        }
+        <>
+            <form method="dialog">
+                <button
+                    className="btn btn-md btn-circle btn-ghost absolute right-2 top-2"
+                    onClick={() => resetState()}
+                >
+                    ✕
+                </button>
+            </form>
+            <form onSubmit={(e) => onSubmit(e)}>
+                {/* headshot */}
+                <div className="tabs tabs-lift">
+                    <input
+                        type="radio"
+                        name="general"
+                        className="tab"
+                        aria-label="General"
+                        checked={tab === "general"}
+                        onChange={() => setTab("general")}
                     />
-                </div>
-                <input
-                    type="radio"
-                    name="private"
-                    className="tab"
-                    aria-label="Private"
-                    checked={tab === "private"}
-                    onChange={() => setTab("private")}
-                />
-                <div className="tab-content bg-base-100 border-base-300 p-6">
-                    <PrivateSubForm
-                        data={privateFormData}
-                        changeData={(
-                            person: "personal" | "guardian1" | "guardian2",
-                            field: string,
-                            value: string
-                        ) =>
-                            setPrivateFormData({
-                                personal: { ...privateFormData["personal"] },
-                                guardian1: { ...privateFormData["guardian1"] },
-                                guardian2: { ...privateFormData["guardian2"] },
-                                [person]: {
-                                    ...privateFormData[person],
+                    <div className="tab-content bg-base-100 border-base-300 p-6">
+                        <GeneralSubForm
+                            data={generalFormData}
+                            changeData={(field: string, value: string) =>
+                                setGeneralFormData({
+                                    ...generalFormData,
                                     [field]: value,
-                                },
-                            })
-                        }
+                                })
+                            }
+                        />
+                    </div>
+                    <input
+                        type="radio"
+                        name="private"
+                        className="tab"
+                        aria-label="Private"
+                        checked={tab === "private"}
+                        onChange={() => setTab("private")}
                     />
+                    <div className="tab-content bg-base-100 border-base-300 p-6">
+                        <PrivateSubForm
+                            data={privateFormData}
+                            changeData={(
+                                person: "personal" | "guardian1" | "guardian2",
+                                field: string,
+                                value: string
+                            ) =>
+                                setPrivateFormData({
+                                    personal: {
+                                        ...privateFormData["personal"],
+                                    },
+                                    guardian1: {
+                                        ...privateFormData["guardian1"],
+                                    },
+                                    guardian2: {
+                                        ...privateFormData["guardian2"],
+                                    },
+                                    [person]: {
+                                        ...privateFormData[person],
+                                        [field]: value,
+                                    },
+                                })
+                            }
+                        />
+                    </div>
                 </div>
-            </div>
-            <div
-                className={`flex justify-end gap-4 mt-4 ${
-                    tab === "private" ? "pb-6" : ""
-                }`}
-            >
-                <button
-                    className="btn btn-neutral"
-                    type="submit"
-                    disabled={validateCreateStudentForm(
-                        generalFormData,
-                        privateFormData
-                    )}
+                <div
+                    className={`flex justify-end gap-4 mt-4 ${
+                        tab === "private" ? "pb-6" : ""
+                    }`}
                 >
-                    Submit
-                </button>
-                <button
-                    type="button"
-                    className="btn"
-                    onClick={() => {
-                        closeModal();
-                        resetState();
-                    }}
-                >
-                    Cancel
-                </button>
-            </div>
-        </form>
+                    <button
+                        className="btn btn-neutral"
+                        type="submit"
+                        disabled={validateCreateStudentForm(
+                            generalFormData,
+                            privateFormData
+                        )}
+                    >
+                        Submit
+                    </button>
+                    <button
+                        type="button"
+                        className="btn"
+                        onClick={() => {
+                            closeModal();
+                            resetState();
+                        }}
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </>
     );
 }
--- new-frontend/src/components/Modal.tsx
+++ new-frontend/src/components/Modal.tsx
@@ -1,15 +1,7 @@
 export default function Modal({ children }) {
     return (
         <dialog id="Modal" className="modal modal-middle">
-            <div className="modal-box flex justify-center">
-                <form method="dialog">
-                    {/* if there is a button in form, it will close the modal */}
-                    <button className="btn btn-md btn-circle btn-ghost absolute right-2 top-2">
-                        ✕
-                    </button>
-                </form>
-                {children}
-            </div>
+            <div className="modal-box flex justify-center">{children}</div>
         </dialog>
     );
 }
