8f1d4a0a8af5fda7dcffab368211d92afe0012a1
Mouzone <sunnyliu010@gmail.com>
2025-04-12 13:25:27 -0400

    Get basic accordian layout with general things needed to render

--- new-frontend/src/components/SubForms/AccountManagementSubForms/PermissionsSubForm.tsx
+++ new-frontend/src/components/SubForms/AccountManagementSubForms/PermissionsSubForm.tsx
@@ -1,11 +1,66 @@
 export default function PermissionsSubForm({ staff, setStaff }) {
     return (
-        <>
+        <form>
             <fieldset className="fieldset w-s bg-base-200 border border-base-300 p-4 rounded-box">
                 <legend className="fieldset-legend">Permissions</legend>
-                <label className="fieldset-label">Sermon Attendance</label>
-                {/* accordian upon accordians: First, Last (email) > Private, Invite > fine tuned permissions for each */}
+                <div className="join join-vertical bg-base-100">
+                    {Object.entries(staff).map(([staffId, staffData]) => (
+                        <div
+                            key={staffId}
+                            className="collapse collapse-arrow join-item border-base-300 border"
+                        >
+                            <input type="radio" name="my-accordion-4" />
+                            <div className="collapse-title font-semibold">
+                                {`${staffData["First Name"]} ${staffData["Last Name"]} (${staffData["Email"]})`}
+                            </div>
+                            {/* private */}
+                            <div className="collapse-content flex flex-col">
+                                <div className="flex">
+                                    <div className="text-sm">
+                                        Can access "Private"
+                                    </div>
+                                    <input
+                                        type="checkbox"
+                                        className="toggle"
+                                        checked={staffData["Private"]}
+                                        onChange={() =>
+                                            setStaff({
+                                                ...staff,
+                                                [staffId]: {
+                                                    ...staffData,
+                                                    Private:
+                                                        !staffData["Private"],
+                                                },
+                                            })
+                                        }
+                                    />
+                                </div>
+                                <div className="flex">
+                                    <div className="text-sm">
+                                        Can invite other accounts
+                                    </div>
+                                    <input
+                                        type="checkbox"
+                                        className="toggle"
+                                        checked={staffData["Invite"]}
+                                        onChange={() =>
+                                            setStaff({
+                                                ...staff,
+                                                [staffId]: {
+                                                    ...staffData,
+                                                    Invite: !staffData[
+                                                        "Private"
+                                                    ],
+                                                },
+                                            })
+                                        }
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </fieldset>
-        </>
+        </form>
     );
 }
