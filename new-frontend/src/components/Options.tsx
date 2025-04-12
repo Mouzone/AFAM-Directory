1999766fe7f7815c08f92e839c5862e2794bb92e
Mouzone <sunnyliu010@gmail.com>
2025-04-11 22:11:25 -0400

    Add account option

--- new-frontend/src/components/Options.tsx
+++ new-frontend/src/components/Options.tsx
@@ -1,13 +1,24 @@
 import Image from "next/image";
 import plus from "../../public/svgs/plus.svg";
+import account from "../../public/svgs/account.svg";
 
-export default function Options({ showAddStudent }) {
+export default function Options({ showAddStudent, showAccounts }) {
     return (
         <ul className="menu menu-sm menu-horizontal bg-base-200 rounded-box mt-6">
             <li>
                 <a className="tooltip" data-tip="Add" onClick={showAddStudent}>
                     <Image src={plus} alt="add" />
                     <span>Add</span>
+                </a>
+            </li>
+            <li>
+                <a
+                    className="tooltip"
+                    data-tip="Accounts"
+                    onClick={showAccounts}
+                >
+                    <Image src={account} alt="accounts" />
+                    <span>Accounts</span>
                 </a>
             </li>
         </ul>
