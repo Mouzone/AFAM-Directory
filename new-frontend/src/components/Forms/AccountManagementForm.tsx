199996abec229f52580f534ea4be9fdc3967b581
Mouzone <sunnyliu010@gmail.com>
2025-04-11 22:47:13 -0400

    Add tabs layout to account modal

--- new-frontend/src/components/Forms/AccountManagementForm.tsx
+++ new-frontend/src/components/Forms/AccountManagementForm.tsx
@@ -2,10 +2,11 @@ import closeModal from "@/utility/closeModal";
 import { FormEvent, useState } from "react";
 import PermissionsSubForm from "../SubForms/AccountManagementSubForms/PermissionsSubForm";
 import InviteSubForm from "../SubForms/AccountManagementSubForms/InviteSubForm";
+import Tab from "../Tab";
 
 export default function AccountManagementForm({ staff, setStaff }) {
     const [staffFormState, setStaffFormState] = useState(staff);
-
+    const [tab, setTab] = useState("permissions");
     // actually log the changes to firestore and update local state
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -23,9 +24,15 @@ export default function AccountManagementForm({ staff, setStaff }) {
             </form>
             <form onSubmit={(e) => onSubmit(e)}>
                 {/* pass in setState to change permissions */}
-                <PermissionsSubForm />
                 {/* pass in setState to add another if valid */}
-                <InviteSubForm />
+                <div className="tabs tabs-lift">
+                    <Tab currTab={tab} value="permissions" setTab={setTab}>
+                        <PermissionsSubForm />
+                    </Tab>
+                    <Tab currTab={tab} value="permissions" setTab={setTab}>
+                        <InviteSubForm />
+                    </Tab>
+                </div>
             </form>
         </>
     );
--- new-frontend/src/components/SubForms/AccountManagementSubForms/PermissionsSubForm.tsx
+++ new-frontend/src/components/SubForms/AccountManagementSubForms/PermissionsSubForm.tsx
@@ -1,3 +1,10 @@
 export default function PermissionsSubForm() {
-    return <></>;
+    return (
+        <>
+            <fieldset className="fieldset w-s bg-base-200 border border-base-300 p-4 rounded-box">
+                <legend className="fieldset-legend">Permissions</legend>
+                <label className="fieldset-label">Sermon Attendance</label>
+            </fieldset>
+        </>
+    );
 }
