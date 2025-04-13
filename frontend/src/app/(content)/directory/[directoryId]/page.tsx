97fcc7f16a63bccff1cc18f6ae5fef01d428858d
Mouzone <sunnyliu010@gmail.com>
2025-04-12 22:43:26 -0400

    Add another debugging statement

--- frontend/src/app/(content)/directory/[directoryId]/page.tsx
+++ frontend/src/app/(content)/directory/[directoryId]/page.tsx
@@ -32,6 +32,7 @@ export default function Page() {
     const [staff, setStaff] = useState<StaffObject>({});
     const [showDeleteStudents, setShowDeleteStudents] = useState(false);
 
+    console.log("directories");
     useEffect(() => {
         if (pathname) {
             const segments = pathname.split("/");
