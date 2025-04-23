5c130fd40d065c55c6278526f14cd49c620735fe
Mouzone <sunnyliu010@gmail.com>
2025-04-23 19:29:08 -0400

    Add million to next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import MillionLint from "@million/lint";
 
 const nextConfig: NextConfig = {
     images: {
@@ -15,4 +16,4 @@ const nextConfig: NextConfig = {
     },
 };
 
-export default nextConfig;
+export default MillionLint.next({ rsc: true })(nextConfig);
