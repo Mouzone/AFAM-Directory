77a58e26c01d3e632f5afef33e447e788a7be6fe
Mouzone <sunnyliu010@gmail.com>
2025-05-03 22:17:02 -0400

    Remove setting of state after submit, which messes up if the user selects the same student again

--- frontend/src/components/Forms/StudentForm.tsx
+++ frontend/src/components/Forms/StudentForm.tsx
@@ -213,8 +213,6 @@ export default function StudentForm({
             }
         }
 
-        setGeneralFormData(generalFormDataDefault);
-        setPrivateFormData(privateFormDataDefault);
         exit();
     };
 
