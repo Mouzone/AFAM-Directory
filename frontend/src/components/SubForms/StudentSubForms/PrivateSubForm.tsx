5f84d8a4fdf2fd9acfcd2b07f77a327f6a858d76
Mouzone <sunnyliu010@gmail.com>
2025-05-09 18:17:29 -0400

    Change Personal Address fields to text inputs

--- frontend/src/components/SubForms/StudentSubForms/PrivateSubForm.tsx
+++ frontend/src/components/SubForms/StudentSubForms/PrivateSubForm.tsx
@@ -3,6 +3,7 @@ import formatText from "@/utility/formatters/formatText";
 import { StudentPrivateInfo } from "@/utility/types";
 import React, { SetStateAction } from "react";
 import { ClipboardIcon } from "@heroicons/react/24/outline";
+import TextInput from "@/components/Inputs/TextInput";
 
 type PrivateSubFormProps = {
     data: StudentPrivateInfo;
@@ -42,109 +43,31 @@ export default function PrivateSubForm({
         <>
             <fieldset className="fieldset w-s bg-base-200 border border-base-300 p-4 rounded-box flex flex-col">
                 <legend className="fieldset-legend">Address</legend>
-
-                <div className="flex flex-col">
-                    <label className="fieldset-label mb-1">
-                        Street Address
-                    </label>
-                    <div className="w-full max-w-[16rem]">
-                        <div className="relative">
-                            <label
-                                htmlFor="npm-install-copy-button"
-                                className="sr-only"
-                            >
-                                NPM Install
-                            </label>
-                            <input
-                                id="npm-install-copy-button"
-                                type="text"
-                                value="npm install flowbite"
-                                readOnly
-                                disabled
-                                className="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            />
-                            <button
-                                onClick={async () =>
-                                    await navigator.clipboard.writeText(
-                                        data["Personal"]["Street Address"]
-                                    )
-                                }
-                                className="absolute end-2 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg p-2 inline-flex items-center justify-center"
-                                aria-label="Copy to clipboard"
-                            >
-                                {!copied ? (
-                                    <svg
-                                        className="w-3.5 h-3.5"
-                                        fill="currentColor"
-                                        viewBox="0 0 18 20"
-                                    >
-                                        <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        className="w-3.5 h-3.5 text-blue-700 dark:text-blue-500"
-                                        fill="none"
-                                        viewBox="0 0 16 12"
-                                    >
-                                        <path
-                                            stroke="currentColor"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M1 5.917 5.724 10.5 15 1.5"
-                                        />
-                                    </svg>
-                                )}
-                            </button>
-                            <div
-                                className={`absolute z-10 inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs tooltip dark:bg-gray-700 ${
-                                    copied
-                                        ? "opacity-100 visible"
-                                        : "opacity-0 invisible"
-                                }`}
-                                style={{
-                                    top: "calc(100% + 0.5rem)",
-                                    right: "0",
-                                }}
-                            >
-                                {copied ? "Copied!" : "Copy to clipboard"}
-                                <div
-                                    className="tooltip-arrow"
-                                    data-popper-arrow
-                                ></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <TextInput
+                    label={"Street Address"}
+                    data={data["Personal"]["Street Address"]}
+                    setData={(e) =>
+                        changeData("Personal", "Street Adress", e.target.value)
+                    }
+                />
                 <div className="flex gap-4">
-                    <div className="flex flex-col">
-                        <label className="fieldset-label">City</label>
-                        <input
-                            type="text"
-                            className="input"
-                            value={formatText(data["Personal"]["City"])}
-                            onChange={(e) =>
-                                changeData("Personal", "City", e.target.value)
+                    <TextInput
+                        label="City"
+                        data={data["Personal"]["City"]}
+                        setData={(e) =>
+                            changeData("Personal", "City", e.target.value)
+                        }
+                    />
+                    <TextInput
+                        label="Zip Code"
+                        data={data["Personal"]["Zip Code"]}
+                        setData={(e) => {
+                            const value = e.target.value;
+                            if (/^\d{0,5}$/.test(value)) {
+                                changeData("Personal", "Zip Code", value);
                             }
-                        />
-                    </div>
-
-                    <div className="flex flex-col">
-                        <label className="fieldset-label">Zip Code</label>
-                        <input
-                            type="text"
-                            inputMode="numeric"
-                            className="input"
-                            value={data["Personal"]["Zip Code"]}
-                            onChange={(e) => {
-                                const value = e.target.value;
-                                if (/^\d{0,5}$/.test(value)) {
-                                    changeData("Personal", "Zip Code", value);
-                                }
-                            }}
-                            maxLength={5}
-                        />
-                    </div>
+                        }}
+                    />
                 </div>
             </fieldset>
 
