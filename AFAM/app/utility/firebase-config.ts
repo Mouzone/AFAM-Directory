0c2e6b49af7a61163f390c8fde85e87da5a4a050
Mouzone <sunnyliu010@gmail.com>
2025-05-30 22:20:10 -0400

    Add personal firebase config data

--- AFAM/app/utility/firebase-config.ts
+++ AFAM/app/utility/firebase-config.ts
@@ -2,14 +2,12 @@ import { initializeApp } from "firebase/app";
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "project-id.firebaseapp.com",
-    databaseURL: "https://project-id.firebaseio.com",
-    projectId: "project-id",
-    storageBucket: "project-id.appspot.com",
-    messagingSenderId: "sender-id",
-    appId: "app-id",
-    measurementId: "G-measurement-id",
+    apiKey: "AIzaSyBy-n3eCWBww4TeI2rOVP7wa6tCsVa2uBo",
+    authDomain: "afam-directory.firebaseapp.com",
+    projectId: "afam-directory",
+    storageBucket: "afam-directory.firebasestorage.app",
+    messagingSenderId: "260596977856",
+    appId: "1:260596977856:web:b86ee3edbc41967d271d25",
 };
 
 const app = initializeApp(firebaseConfig);
