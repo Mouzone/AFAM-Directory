4939d8ce7bbd74fefb0b382d72557b8e4adabe50
Liuser <sunnyliu@sunnys-air.mynetworksettings.com>
2025-06-30 12:31:06 -0400

    Remove unnecessary imports and format

--- frontend/src/app/newcomer-form/layout.tsx
+++ frontend/src/app/newcomer-form/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "../globals.css";
-import QueryProvider from "../../components/Providers/QueryProvider";
-import { AuthProvider } from "../../components/Providers/AuthProvider";
 
 export const metadata: Metadata = {
     title: "Directory",
