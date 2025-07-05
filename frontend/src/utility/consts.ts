d5817e2ae4a583afb74ac989d882beff359f06a1
Liuser <sunnyliu@sunnys-air.mynetworksettings.com>
2025-07-04 23:07:28 -0400

    Add to mandatory fields

--- frontend/src/utility/consts.ts
+++ frontend/src/utility/consts.ts
@@ -1,39 +1,42 @@
 import { StudentGeneralInfo } from "./types";
 
 export const generalFormDataDefault = {
-    Id: "",
-    "Headshot URL": "",
-    "First Name": "",
-    "Last Name": "",
-    Gender: "M",
-    Birthday: new Date().toISOString().split("T")[0],
-    "High School": "",
-    Grade: 9,
-    Teacher: "None",
-    Allergies: [],
-    "First Time": true,
+	Id: "",
+	"Headshot URL": "",
+	"First Name": "",
+	"Last Name": "",
+	Gender: "M",
+	Birthday: new Date().toISOString().split("T")[0],
+	"High School": "",
+	Grade: 9,
+	Teacher: "None",
+	Allergies: [],
+	"First Time": true,
 } as StudentGeneralInfo;
 
 export const privateFormDataDefault = {
-    Personal: {
-        "Street Address": "",
-        City: "",
-        "Zip Code": "",
-        Phone: "",
-        Email: "",
-    },
-    "Guardian 1": { "First Name": "", "Last Name": "", Phone: "", Email: "" },
-    "Guardian 2": { "First Name": "", "Last Name": "", Phone: "", Email: "" },
+	Personal: {
+		"Street Address": "",
+		City: "",
+		"Zip Code": "",
+		Phone: "",
+		Email: "",
+	},
+	"Guardian 1": { "First Name": "", "Last Name": "", Phone: "", Email: "" },
+	"Guardian 2": { "First Name": "", "Last Name": "", Phone: "", Email: "" },
 };
 
 export const schema = Object.keys(generalFormDataDefault).filter(
-    (key) => key !== "Headshot URL"
+	(key) => key !== "Headshot URL"
 ) as (keyof StudentGeneralInfo)[];
 
 export const mandatoryGeneralFields: (keyof StudentGeneralInfo)[] = [
-    "First Name",
-    "Last Name",
-    "Birthday",
-    "Grade",
-    "Teacher",
+	"First Name",
+	"Last Name",
+	"Birthday",
+	"Grade",
+	"Teacher",
+	"High School",
+	"First Time",
+	"Gender",
 ];
