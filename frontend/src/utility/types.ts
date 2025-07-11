5cc3f7994e27542c9ebc9e5a2d6c4c1d116cfdd3
Liuser <sunnyliu@liusers-air.mynetworksettings.com>
2025-07-10 20:19:53 -0400

    Change form in how it renders isNewcomer and the typing assoicated with it

--- frontend/src/utility/types.ts
+++ frontend/src/utility/types.ts
@@ -1,11 +1,11 @@
 import { User } from "firebase/auth";
 
 export type AccountInfo = {
-    Email: string;
-    "First Name": string;
-    "Last Name": string;
-    Private: boolean;
-    "Manage Accounts": boolean;
+	Email: string;
+	"First Name": string;
+	"Last Name": string;
+	Private: boolean;
+	"Manage Accounts": boolean;
 };
 
 // loading, loaded, logged out / error
@@ -13,64 +13,64 @@ export type AuthUser = null | User | false;
 export type Grade = 9 | 10 | 11 | 12;
 
 export type StudentGeneralInfo = {
-    Id: string;
-    "Headshot URL": string;
-    "First Name": string;
-    "Last Name": string;
-    Gender: "M" | "F";
-    Birthday: string;
-    "High School": string;
-    Grade: Grade;
-    Teacher: "None" | string;
-    Allergies: string[];
-    "First Time"?: boolean;
+	Id: string;
+	"Headshot URL": string;
+	"First Name": string;
+	"Last Name": string;
+	Gender: "M" | "F";
+	Birthday: string;
+	"High School": string;
+	Grade: Grade;
+	Teacher: "None" | string;
+	Allergies: string[];
+	"First Time": string;
 };
 
 export type Guardian = {
-    "First Name": string;
-    "Last Name": string;
-    Phone: string;
-    Email: string;
+	"First Name": string;
+	"Last Name": string;
+	Phone: string;
+	Email: string;
 };
 export type StudentPrivateInfo = {
-    Personal: {
-        "Street Address": string;
-        City: string;
-        "Zip Code": string;
-        Phone: string;
-        Email: string;
-    };
-    "Guardian 1": Guardian;
-    "Guardian 2": Guardian;
+	Personal: {
+		"Street Address": string;
+		City: string;
+		"Zip Code": string;
+		Phone: string;
+		Email: string;
+	};
+	"Guardian 1": Guardian;
+	"Guardian 2": Guardian;
 };
 
 export type Attendance = {
-    "Sermon Attendance": boolean;
-    "Class Attendance": boolean;
+	"Sermon Attendance": boolean;
+	"Class Attendance": boolean;
 };
 
 export type AttendanceObject = { [key: string]: Attendance };
 
 export type Staff = {
-    "First Name": string;
-    "Last Name": string;
-    Email: string;
-    Private: boolean;
-    "Manage Accounts": boolean;
+	"First Name": string;
+	"Last Name": string;
+	Email: string;
+	Private: boolean;
+	"Manage Accounts": boolean;
 };
 
 export type StaffObject = { [key: string]: Staff };
 
 export type SearchTerms = {
-    "First Name": string;
-    "Last Name": string;
-    Gender: "M" | "F";
-    "High School": string;
-    Grade: "9" | "10" | "11" | "!2";
-    Teacher: "None" | string;
+	"First Name": string;
+	"Last Name": string;
+	Gender: "M" | "F";
+	"High School": string;
+	Grade: "9" | "10" | "11" | "!2";
+	Teacher: "None" | string;
 };
 
 export type ToastContextType = {
-    message: string;
-    setMessage: React.Dispatch<React.SetStateAction<string>>;
+	message: string;
+	setMessage: React.Dispatch<React.SetStateAction<string>>;
 };
